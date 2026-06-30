ee277ded04dc41b378d2df796005eb705678a9df
NagaLokesh Battula <nagalokesh@paithacs.com>
2026-06-30 16:49:25 +0530

    Implement switch statement for day of the week

---
+++ switchstatement.js
@@ -0,0 +1,55 @@
+    // let day =3 ;
+
+// if (day ==1){
+//     console.log("Mopday")
+// } else if (day =2){
+//     console.log("Tuesday")
+// }
+
+// Switch Case (Clean Decision Making)
+
+// syntax
+
+// switch(expression){
+//     case value1:
+//         //code
+//         break;
+//     case valu22:
+//         //code
+//         break;
+//     default:
+//         //code
+// }
+
+let day=7;
+switch(day){
+    case 1:
+        console.log("sunday");
+        break;
+    case 2:
+        console.log("monday");
+        break;
+    case 3:
+        console.log("tuesday");
+        break;
+    case 4:
+        console.log("wednesday");
+        break;
+    case 5:
+        console.log("thursday");
+        break;
+    case 6:
+        console.log("friday");
+        break;
+    case 7:
+        console.log("saturday");
+        break;
+    default:
+        console.log("invalid number");
+        
+        
+
+
+        
+        
+}
